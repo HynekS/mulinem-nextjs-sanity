7837741602c80d71a89221e52b11526c6e56852e
HynekS <hyneksvacha@seznam.cz>
2022-07-07 14:28:42 +0200

    Add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
-import type { AppProps } from "next/app";
+import Head from "next/head";
 import Layout from "@components/Layout";
 import "node_modules/modern-normalize/modern-normalize.css";
 import "../styles/globals.css";
 
+import type { AppProps } from "next/app";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+        <link rel="icon" type="image/png" href="favicon.png" />
+      </Head>
       <Layout {...pageProps}>
         <Component {...pageProps} />
       </Layout>
