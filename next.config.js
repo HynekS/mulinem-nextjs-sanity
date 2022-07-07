f76a8c1c8bc9d271b44b5b296faac47d5ba63555
HynekS <hyneksvacha@seznam.cz>
2022-07-07 10:41:19 +0200

    Add Preact aliasing

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
